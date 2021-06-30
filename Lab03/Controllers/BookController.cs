3473cd48a1d4dba180e40802f7e8c8b59eab0424
HuyHuu <86702787+HuyHuu@users.noreply.github.com>
2021-06-30 11:48:24 +0700

    Initial commit

---
+++ Lab03/Controllers/BookController.cs
@@ -0,0 +1,78 @@
+﻿using Lab03.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab03.Controllers
+{
+    public class BookController : Controller
+    {
+        
+        
+        // GET: Book
+        public ActionResult ListBook()
+        {
+            
+            BookManagerContext context = new BookManagerContext();
+            var listbook = context.Books.ToList();          
+            return View(listbook);
+        }
+        [Authorize]
+        public ActionResult Buy(int id)
+        {
+            BookManagerContext context = new BookManagerContext();
+            Book book = context.Books.SingleOrDefault(p => p.ID == id);
+            if(book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+        [Authorize]
+        public ActionResult Create()
+        {
+            BookManagerContext context = new BookManagerContext();
+            var listBook = context.Books.ToList();
+            return View();
+
+        }
+        [Authorize]
+        public ActionResult Edit(int id)
+        {
+            
+            BookManagerContext context = new BookManagerContext();
+            var listBook = context.Books.ToList();
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            Book book = listBook.Find(p => p.ID == id);
+            if (book == null)
+                return HttpNotFound();
+
+            return View(book);
+            
+        }
+        [Authorize]
+        public ActionResult Delete(int id)
+        {
+            
+            
+            
+            BookManagerContext context = new BookManagerContext();
+            var listBook = context.Books.ToList();
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            Book book = listBook.Find(p => p.ID == id);
+            if (book == null)
+                return HttpNotFound();
+
+            return View(book);
+        }
+
+    }
+}
---
+++ Lab03/Models/Book.cs
@@ -0,0 +1,36 @@
+﻿namespace Lab03.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Book")]
+    public partial class Book
+    {
+        [Required(ErrorMessage ="ID không được để trống")]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage ="Tiêu đề không được để trống")]
+        [StringLength(100, ErrorMessage ="Tiêu đề không quá 100 kí tự" )]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Nội dung không được để trống")]
+        [StringLength(255)]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Tên không được để trống")]
+        [StringLength(30, ErrorMessage = "Tác giả không quá 30 kí tự")]
+        public string Author { get; set; }
+
+        [Required(ErrorMessage = "Hình ảnh không được để trống")]
+        [StringLength(255)]
+        public string Images { get; set; }
+
+        [Required(ErrorMessage = "Giá không được để trống")]
+        [Range(1000, 1000000, ErrorMessage = "Giá sách từ 1000 đến 1000000")]
+        public int Price { get; set; }
+    }
+}
